94de833724c799d46b9d6ae9eebac2d7fce30aee
Mremza <200706535@student.uj.ac.za>
2017-09-20 17:18:40 +0200

    I added a create custom goal cs file

---
+++ CreateCustomGoal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalFinanceManagement
+{
+    public class CreateCustomGoal
+    {
+        ReadFinancialApplicationService readCustomGoalService = new ReadFinancialApplicationService();
+        FinancialManagementService customGoalService = new FinancialManagementService();
+
+        public string createCustomGoal(CustomGoal saveCustomGoal)
+        {
+            bool checkCustomGoalSavingName = readCustomGoalService.checkUserCustomGoalExistence(saveCustomGoal.getCustomGoalName());
+
+            if(checkCustomGoalSavingName != true)
+            {
+                if(customGoalService .createCustomGoalSaving (saveCustomGoal.getCustomGoalName(), saveCustomGoal.getCustomGoalTarget (), saveCustomGoal.getCustomGoalPriority (), saveCustomGoal.getCustomGoalContributionType (), saveCustomGoal.getCustomGoalDueDate ()) != false )
+                {
+                    return alertMessage("Custom Goal Saving was successfully created");
+                }
+                else
+                {
+                    return alertMessage("Custom Goal Saving was NOT successfully created");
+                }
+            }
+            else if(checkCustomGoalSavingName != false)
+            {
+                return alertMessage("Custom Goal name already exists.......");
+            }
+
+            return "";
+        }
+
+        public string alertMessage(string custom_message)
+        {
+            return custom_message;
+        }
+
+        public bool createCustomGoalSuccessfully(string successCustomGoal, int custID)
+        {
+            return readCustomGoalService.createCustomGoalSuccessfully(successCustomGoal, custID);
+        }
+
+    }
+}
