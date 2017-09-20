b15b37867a28d5bb2b2d0f2aa18b6014fb7ccda7
Mremza <200706535@student.uj.ac.za>
2017-09-20 17:34:47 +0200

    I have added an emergency saving class file

---
+++ Emergency.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalFinanceManagement
+{
+    public class Emergency
+    {
+        private string emergencyName;
+        private double  emergencyTarget;
+        private int emergencyPriority;
+        private string emergencySaveOption;
+
+        public string getEmergencyName()
+        {
+            return emergencyName;
+        }
+
+        public double  getEmergencyTarget()
+        {
+            return emergencyTarget;
+        }
+
+        public int getEmergencyPriority()
+        {
+            return emergencyPriority;
+        }
+
+        public string getEmergencySaveOption()
+        {
+            return emergencySaveOption;
+        }
+
+
+        public void setEmergencyDetails(string emerName, double  emerTarget, int emerPriority, string emerSaveOpt)
+        {
+            this.emergencyName = emerName;
+            this.emergencyTarget = emerTarget;
+            this.emergencyPriority = emerPriority;
+            this.emergencySaveOption = emerSaveOpt;
+        }
+    }
+}
