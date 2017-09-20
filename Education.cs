582c9413844db9a0f9350b0aa0ffb1aafa421f68
Mremza <200706535@student.uj.ac.za>
2017-09-20 17:33:59 +0200

    I have added an education saving class file

---
+++ Education.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalFinanceManagement
+{
+    public class Education
+    {
+        private string institutionName;
+        private string courseName;
+        private DateTime startStudyingDate;
+        private DateTime endStudyingDate;
+        private int educationPriority;
+        private double educationTarget;
+        private DateTime educationDueSaving;
+        private DateTime educationStartSaving;
+        private string educationContributionType;
+
+        public string getInstitutionName()
+        {
+            return institutionName;
+        }
+
+        public string getCourseName()
+        {
+            return courseName;
+        }
+
+        public DateTime getStartStudyingDate()
+        {
+            return startStudyingDate;
+        }
+
+        public DateTime getEndStudyingDate()
+        {
+            return endStudyingDate;
+        }
+
+        public int getEducationPrioriry()
+        {
+            return educationPriority;
+        }
+
+        public double getEducationTarget()
+        {
+            return educationTarget;
+        }
+
+        public DateTime getEducationStartSaving()
+        {
+            return educationStartSaving;
+        }
+
+        public DateTime getEducationEndSaving()
+        {
+            return educationDueSaving;
+        }
+
+        public string getEducationContributionType()
+        {
+            return educationContributionType;
+        }
+
+
+        public void setEducationDetails(string instName, string courName, DateTime startStudy, DateTime endStudy, int eduPriority, double eduTarget,  string eduContribution, DateTime startSave, DateTime endSave)
+        {
+            this.institutionName = instName;
+            this.courseName = courName;
+            this.startStudyingDate = startStudy;
+            this.endStudyingDate = endStudy;
+            this.educationPriority = eduPriority;
+            this.educationTarget = eduTarget;
+            this.educationStartSaving = startSave;
+            this.educationDueSaving = endSave;
+            this.educationContributionType = eduContribution;
+        }
+
+    }
+}
