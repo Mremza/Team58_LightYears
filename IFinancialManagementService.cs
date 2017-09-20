542ce10a13bc2f65fe861bde51219b033f33bea1
Mremza <200706535@student.uj.ac.za>
2017-09-20 17:39:53 +0200

    Added Financial Management Service interface cs

---
+++ IFinancialManagementService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PersonalFinanceManagement
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFinancialManagementService" in both code and config file together.
+    [ServiceContract]
+    public interface IFinancialManagementService
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        bool creatBudget(string budgetName, double amount_to_spend, DateTime budgetStart, DateTime budgetEnd, double assign_amount);
+
+        [OperationContract]
+        bool createExpense(string expenseName, DateTime expensePur_date, double expense_price, int priority);
+
+        [OperationContract]
+        bool AddUser(String name, String surname, String username, String password, DateTime DOB, String email);
+    }
+
+
+}
