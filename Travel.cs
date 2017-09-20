8a279a911bbca2fb492c8bc0cf079058bf81a2d4
Mremza <200706535@student.uj.ac.za>
2017-09-20 17:51:40 +0200

    Added a travel saving class file

---
+++ Travel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalFinanceManagement
+{
+    public class Travel
+    {
+
+        private string travelDestination;
+        private string travelType;
+        private int travelDuration;
+        private int travelPriority;
+        private int travelTravelers;
+        private DateTime travelStartSaving;
+        private DateTime travelEndSaving;
+        private string travelContributionType;
+        private double travelTargetAmount;
+
+        public string getTravelDestination()
+        {
+            return travelDestination;
+        }
+
+        public string getTravelType()
+        {
+            return travelType;
+        }
+
+        public int getTravelDuration()
+        {
+            return travelDuration;
+        }
+
+        public int getTravelPriority()
+        {
+            return travelPriority;
+        }
+
+        public int getTravelTravelers()
+        {
+            return travelTravelers;
+        }
+
+        public DateTime getTravelStartSaving()
+        {
+            return travelStartSaving;
+        }
+
+        public DateTime getTravelEndSaving()
+        {
+            return travelEndSaving;
+        }
+
+        public string getTravelContributionType()
+        {
+            return travelContributionType;
+        }
+
+        public double getTravelTargetAmount()
+        {
+            return travelTargetAmount;
+        }
+
+
+
+        public void setTravelDetails(string travDestination, string travType, int travDuration, int travPriority, int travTravelers, DateTime travStartSave, DateTime travEndSave, string travContributionType, double travTargetAmount)
+        {
+            this.travelDestination = travDestination;
+            this.travelType = travType;
+            this.travelDuration = travDuration;
+            this.travelPriority = travPriority;
+            this.travelTravelers = travTravelers;
+            this.travelStartSaving = travStartSave;
+            this.travelEndSaving = travEndSave;
+            this.travelContributionType = travContributionType;
+            this.travelTargetAmount = travTargetAmount;
+        }
+    }
+}
