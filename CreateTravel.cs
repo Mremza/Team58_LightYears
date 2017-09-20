6b3b4a7c3945732dbf6a0444d1d408a84b828f94
Mremza <200706535@student.uj.ac.za>
2017-09-20 17:27:15 +0200

    I added a travel saving cs file

---
+++ CreateTravel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalFinanceManagement
+{
+    public class CreateTravel
+    {
+        ReadFinancialApplicationService readTraveService = new ReadFinancialApplicationService();
+        FinancialManagementService travelService = new FinancialManagementService();
+
+        public string createTravel(Travel saveTravel)
+        {
+            bool checkTravelDestination = readTraveService.checkUserTravelExistence(saveTravel.getTravelDestination());
+            if(checkTravelDestination != true)
+            {
+                if(travelService .createTravelSaving (saveTravel .getTravelDestination (), saveTravel.getTravelType(), saveTravel.getTravelDuration (), saveTravel.getTravelPriority (), saveTravel.getTravelTravelers (), saveTravel.getTravelStartSaving (), saveTravel.getTravelEndSaving (), saveTravel.getTravelContributionType (), saveTravel.getTravelTargetAmount()) != false )
+                {
+                    return alertMessage("Travel Saving is successfully created");
+                }
+                else
+                {
+                    return alertMessage("Travel Saving is NOT successfully created");
+                }
+            }
+            else if(checkTravelDestination != false )
+            {
+                return alertMessage("Travel Destination name already exists.......");
+            }
+
+            return "";
+        }
+
+        public string alertMessage(string travel_message)
+        {
+            return travel_message;
+        }
+
+        public bool createTravelSuccessfully(string successTravel, int travID)
+        {
+            return readTraveService.createTravelSuccessfully(successTravel , travID );
+        }
+    }
+}
